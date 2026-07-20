b7bdd427b0f0e62d9d7c7da62700157095f449cf
jisan <mhhaque4@gmail.com>
2026-07-20 19:42:01 +0600

    fresh commit

---
+++ app/editor/utils/layout.ts
@@ -0,0 +1,43 @@
+import { EditorDocument } from "../types/document";
+import { DocumentBlock } from "../types/block";
+import { RenderPage } from "../types/page";
+import {
+  createPage,
+  PAGE_HEIGHT,
+  PAGE_PADDING_TOP,
+  PAGE_PADDING_BOTTOM,
+} from "../factories/page";
+
+export function buildPages(document: EditorDocument): RenderPage[] {
+  const pages: RenderPage[] = [];
+
+  let pageNumber = 1;
+
+  let currentPage = createPage(`page-${pageNumber}`);
+
+  let currentHeight = PAGE_PADDING_TOP;
+
+  const blocks: DocumentBlock[] = document.blocks ?? [];
+
+  for (const block of blocks) {
+    const blockHeight =
+      Math.max(block.height ?? 40, 40) + (block.marginBottom ?? 0);
+    if (currentHeight + blockHeight > PAGE_HEIGHT - PAGE_PADDING_BOTTOM) {
+      pages.push(currentPage);
+
+      pageNumber++;
+
+      currentPage = createPage(`page-${pageNumber}`);
+
+      currentHeight = PAGE_PADDING_TOP;
+    }
+
+    currentPage.blocks.push(block);
+
+    currentHeight += blockHeight;
+  }
+
+  pages.push(currentPage);
+
+  return pages;
+}
